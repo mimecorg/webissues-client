f7171a09b20bc3933a28ac9cd47f621bd5bbb32e
mimecorg <mimec@mimec.org>
2011-05-08 14:53:13 +0000

    implemented the REGEXP operator
    git-svn-id: svn+ssh://svn.code.sf.net/p/webissues/code/trunk/client@1346 c8fa975d-6fd4-4e76-b3b8-1a99b2ee3549

--- src/sqlite/sqliteextension.cpp
+++ src/sqlite/sqliteextension.cpp
@@ -28,12 +28,35 @@ static int localeCompare( void* /*arg*/, int len1, const void* data1, int len2, 
 {
     QString string1 = QString::fromRawData( reinterpret_cast<const QChar*>( data1 ), len1 / sizeof( QChar ) );
     QString string2 = QString::fromRawData( reinterpret_cast<const QChar*>( data2 ), len2 / sizeof( QChar ) );
+
     return QString::localeAwareCompare( string1, string2 );
+}
+
+static void regexpFunction( sqlite3_context* context, int /*argc*/, sqlite3_value** argv )
+{
+    int len1 = sqlite3_value_bytes16( argv[ 0 ] );
+    const void* data1 = sqlite3_value_text16( argv[ 0 ] );
+    int len2 = sqlite3_value_bytes16( argv[ 1 ] );
+    const void* data2 = sqlite3_value_text16( argv[ 1 ] );
+
+    if ( !data1 || !data2 )
+        return;
+
+    QString string1 = QString::fromRawData( reinterpret_cast<const QChar*>( data1 ), len1 / sizeof( QChar ) );
+    QString string2 = QString::fromRawData( reinterpret_cast<const QChar*>( data2 ), len2 / sizeof( QChar ) );
+
+    QRegExp pattern( string1, Qt::CaseInsensitive );
+
+    bool match = pattern.exactMatch( string2 );
+
+    sqlite3_result_int( context, match ? 1 : 0 );
 }
 
 void installSQLiteExtension( const QVariant& handle )
 {
-    sqlite3* ptr = *static_cast<sqlite3* const*>( handle.data() );
+    sqlite3* db = *static_cast<sqlite3* const*>( handle.data() );
 
-    sqlite3_create_collation( ptr, "LOCALE", SQLITE_UTF16, NULL, &localeCompare );
+    sqlite3_create_collation( db, "LOCALE", SQLITE_UTF16, NULL, &localeCompare );
+
+    sqlite3_create_function( db, "regexp", 2, SQLITE_UTF16, NULL, &regexpFunction, NULL, NULL );
 }
